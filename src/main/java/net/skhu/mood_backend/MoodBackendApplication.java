b38bcc5efbf6c48e77477fdaae4e028db24d135e
giwoong <dev.choi0409@gmail.com>
2024-10-28 19:31:44 +0900

    project setting

---
+++ src/main/java/net/skhu/mood_backend/MoodBackendApplication.java
@@ -0,0 +1,13 @@
+package net.skhu.mood_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoodBackendApplication.class, args);
+    }
+
+}
